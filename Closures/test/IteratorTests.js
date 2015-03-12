f85098d8b7b835f039d46f61c1269e7c5efadd98
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-03-12 17:10:14 +0000

    broken iterator tests

---
+++ Closures/test/IteratorTests.js
@@ -0,0 +1,28 @@
+/* global iterator, tddjs */
+
+TestCase("IteratorTest", {
+  "test next should return first item":
+  function () {
+    var collection = [1, 2, 3, 4, 5];
+    assertSame(collection[0], tddjs.iterator.next());
+    assertTrue(iterator.hasNext());
+  },
+  "test hasNext should be false after last item":
+  function () {
+    var collection = [1, 2];
+    var iterator = tddjs.iterator(collection);
+    iterator.next();
+    iterator.next();
+    assertFalse(iterator.hasNext());
+  },
+  "test should loop collection with iterator":
+  function () {
+    var collection = [1, 2, 3, 4, 5];
+    var it = tddjs.iterator(collection);
+    var result = [];
+    while (it.hasNext()) {
+      result.push(it.next());
+}
+    assertEquals(collection, result);
+  }
+});
