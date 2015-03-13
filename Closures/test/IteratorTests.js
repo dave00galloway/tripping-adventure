82e3c05f083d6d340993d4d4bd29c15cf3a095dd
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-03-13 16:52:04 +0000

    added alternate implementations of iterator and fixed tests to use either

--- Closures/test/IteratorTests.js
+++ Closures/test/IteratorTests.js
@@ -1,29 +1,48 @@
 /* global iterator, tddjs */
 
 TestCase("IteratorTest", {
-  "test next should return first item":
-  function () {
-    var collection = [1, 2, 3, 4, 5];
-    var iterator = tddjs.iterator(collection);
-    assertSame(collection[0], iterator.next());
-    assertTrue(iterator.hasNext());
-  },
-  "test hasNext should be false after last item":
-  function () {
-    var collection = [1, 2];
-    var iterator = tddjs.iterator(collection);
-    iterator.next();
-    iterator.next();
-    assertFalse(iterator.hasNext());
-  },
-  "test should loop collection with iterator":
-  function () {
-    var collection = [1, 2, 3, 4, 5];
-    var it = tddjs.iterator(collection);
-    var result = [];
-    while (it.hasNext()) {
-      result.push(it.next());
-}
-    assertEquals(collection, result);
-  }
+	"test next should return first item with java-style iterator" : function() {
+		var collection = [ 1, 2, 3, 4, 5 ];
+		var iterator = tddjs.javastyle.iterator(collection);
+		assertSame(collection[0], iterator.next());
+		assertTrue(iterator.hasNext());
+	},
+	"test hasNext should be false after last item with java-style iterator" : function() {
+		var collection = [ 1, 2 ];
+		var iterator = tddjs.javastyle.iterator(collection);
+		iterator.next();
+		iterator.next();
+		assertFalse(iterator.hasNext());
+	},
+	"test should loop collection with java-style iterator" : function() {
+		var collection = [ 1, 2, 3, 4, 5 ];
+		var it = tddjs.javastyle.iterator(collection);
+		var result = [];
+		while (it.hasNext()) {
+			result.push(it.next());
+		}
+		assertEquals(collection, result);
+	},
+	"test should loop collection with iterator" : function() {
+		var collection = [ 1, 2, 3, 4, 5 ];
+		var next = tddjs.iterator(collection);
+		var result = [];
+		while (next.hasNext) {
+			result.push(next());
+		}
+		assertEquals(collection, result);
+	},
+	"test next should return first item" : function() {
+		var collection = [ 1, 2, 3, 4, 5 ];
+		var next = tddjs.iterator(collection);
+		assertSame(collection[0], next());
+		assertTrue(next.hasNext);
+	},
+	"test hasNext should be false after last item" : function() {
+		var collection = [ 1, 2 ];
+		var next = tddjs.iterator(collection);
+		next();
+		next();
+		assertFalse(next.hasNext);
+	},	
 });
