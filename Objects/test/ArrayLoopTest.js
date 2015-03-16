61bbe6b4dd3342780dea594419611948ee33c291
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-03-16 08:18:00 +0000

    Broken - the global extension to Array causes looping over an array to return its new exgension method as an enumerable item!

---
+++ Objects/test/ArrayLoopTest.js
@@ -0,0 +1,19 @@
+TestCase("ArrayLoopTest", {
+	"test looping should iterate over all items" : function() {
+		var array = [ 1, 2, 3, 4, 5, 6 ];
+		var result = [];
+		// Standard for-loop
+		for (var i = 0, l = array.length; i < l; i++) {
+			result.push(array[i]);
+		}
+		assertEquals("123456", result.join(""));
+	},
+	"test for-in loop should iterate over all items" : function() {
+		var array = [ 1, 2, 3, 4, 5, 6 ];
+		var result = [];
+		for ( var i in array) {
+			result.push(array[i]);
+		}
+		assertEquals("123456", result.join(""));
+	}
+});
---
+++ Objects/test/InheritingAndShadowingProperties.js
@@ -0,0 +1,22 @@
+TestCase("ObjectPropertyTest", {
+	"test setting property shadows property on prototype" : function() {
+		var object1 = {};
+		var object2 = {};
+		// Both objects inherit Object.prototype.toString
+		assertEquals(object1.toString, object2.toString);
+		var chris = {
+			name : "Chris",
+			toString : function() {
+				return this.name;
+			}
+		};
+		// chris object defines a toString property that is
+		// not the same object as object1 inherits from
+		// Object.prototype
+		assertFalse(object1.toString === chris.toString);
+		// Deleting the custom property unshadows the
+		// inherited Object.prototype.toString
+		delete chris.toString;
+		assertEquals(object1.toString, chris.toString);
+	}
+});
