c00bb1e8867eb45498ee633f5df068c2ed45fc4a
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-03-19 15:30:44 +0000

    Implemented inheritance abstraction

--- Objects/src/Sphere.js
+++ Objects/src/Sphere.js
@@ -1,13 +1,20 @@
+//using inherit abstraction
 function Sphere(radius) {
 	this.radius = radius;
 }
-Sphere.prototype = (function() {
-	function F() {
-	}
-	;
-	F.prototype = Circle.prototype;
-	return new F();
-}());
-// Don't forget the constructor - else it will resolve as
-// Circle through the prototype chain
-Sphere.prototype.constructor = Sphere;
+Sphere.inherit(Circle);
+
+//clunky inheritance
+//function Sphere(radius) {
+//	this.radius = radius;
+//}
+//Sphere.prototype = (function() {
+//	function F() {
+//	}
+//	;
+//	F.prototype = Circle.prototype;
+//	return new F();
+//}());
+//// Don't forget the constructor - else it will resolve as
+//// Circle through the prototype chain
+//Sphere.prototype.constructor = Sphere;
