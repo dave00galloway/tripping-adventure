68a7a082ab8187579024e05bb523ae984ff26766
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-03-19 16:01:10 +0000

    moved the LOUD PERSON definition to person class instead of being in th etest

--- Objects/src/Person.js
+++ Objects/src/Person.js
@@ -10,3 +10,14 @@ Person.prototype = {
 		return "Hello";
 	}
 };
+
+function LoudPerson(name) {
+	_super(this, "constructor", name);
+}
+LoudPerson.inherit(Person);
+LoudPerson.prototype.getName = function() {
+	return _super(this, "getName").toUpperCase();
+};
+LoudPerson.prototype.say = function(words) {
+	return _super(this, "speak", words) + "!!!";
+};
