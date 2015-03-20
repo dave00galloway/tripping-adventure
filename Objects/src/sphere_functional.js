87fe7b22123039bdf5340a0af6873465756d808c
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-03-20 21:23:06 +0000

    bit of experimentation with functional inheritance

--- Objects/src/sphere_functional.js
+++ Objects/src/sphere_functional.js
@@ -2,8 +2,23 @@ function sphere(radius) {
 	var sphereObj = circle(radius);
 	var circleArea = sphereObj.area;
 	function area() {
-		return 4 * circleArea.call(this);
+		// stack overflow! return 4 * sphereObj.area.call(this);
+		return 4 * circleArea.call(this); // book solution
+		// var a = sphereObj.area();
+		// return 4 * a;
 	}
 	sphereObj.area = area;
 	return sphereObj;
 }
+
+// The functional style is an interesting alternative to the pseudo-classical
+// con- structors, but comes with its own limitations. The two most obvious
+// drawbacks of this style of coding are that every object keeps its own copy of
+// every function, increasing memory usage, and that in practice we aren’t using
+// the prototype chain, which means more cruft when we want to call the “super”
+// function or something similar.
+
+// limited experimentation here shows that it might be possible to simplify the
+// method chain a bit, but you end up with always calculating the area and
+// storing it as a
+// field, which migh tbe worse than storing a copy of a function
