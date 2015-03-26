b761dda5b0595f445c6e0d9851ba32a29c2821a9
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-03-26 12:42:56 +0000

    fixed the null test, but why?

--- Closures/lib/tddjs_extend.js
+++ Closures/lib/tddjs_extend.js
@@ -1,10 +1,15 @@
 (function() {
-	tddjs.extend = (function() {
-		function extend(target, source) {
-			tddjs.each(source, function(prop, val) {
-				target[prop] = val;
-			});
-		}
-		return extend;
-	}());
+	// previous implementation returned a function, this one returns the
+	// extended object
+	function extend(target, source) {
+		target = target || {};
+		tddjs.each(source, function(prop, val) {
+			target[prop] = val;
+		});
+		return target;
+	}
+	// called during script load only, not during execution
+	if (typeof tddjs == "object") {
+		tddjs.extend = extend;
+	}
 }());
