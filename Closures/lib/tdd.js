88102956a0ee23a4327d6137ced9a47a3048c3fe
dave00galloway <david_a_galloway@yahoo.co.uk>
2015-03-30 10:37:49 +0100

    Getting pretty annoyed at this now. Got the tests working, but not really sure what they prove as I never saw them run to fail. using a locl copy of tdd.js as sharing the global one doesn't seem to work

--- Closures/lib/tdd.js
+++ Closures/lib/tdd.js
@@ -1,123 +1,148 @@
 /*jslint indent: 2, onevar: false, plusplus: false, eqeqeq: false, nomen: false*/
-var tddjs = (function () {
-    function namespace(string) {
-        var object = this;
-        var levels = string.split(".");
+var tddjs = (function() {
+	function namespace(string) {
+		var object = this;
+		var levels = string.split(".");
 
-        for (var i = 0, l = levels.length; i < l; i++) {
-            if (typeof object[levels[i]] == "undefined") {
-                object[levels[i]] = {};
-            }
-            object = object[levels[i]];
-        }
-        return object;
-    }
-    function hello(name) {
-        return "hello " + name;
-    }
-    return {
-        hello: hello
-    };
-    return {
-        namespace: namespace
-    };
+		for (var i = 0, l = levels.length; i < l; i++) {
+			if (typeof object[levels[i]] == "undefined") {
+				object[levels[i]] = {};
+			}
+			object = object[levels[i]];
+		}
+		return object;
+	}
+	function hello(name) {
+		return "hello " + name;
+	}
+	return {
+		hello : hello
+	};
+	return {
+		namespace : namespace
+	};
 }());
 
 tddjs.tag = "random string";
 
 tddjs.hi = function hi() {
-    return "hi";
+	return "hi";
 };
 
-tddjs.isOwnProperty = (function () {
-    var hasOwn = Object.prototype.hasOwnProperty;
+(function() {
+	var dom = tddjs.namespace("dom");
 
-    if (typeof hasOwn == "function") {
-        return function (object, property) {
-            return hasOwn.call(object, property);
-        };
-    } else {
-        // Provide an emulation if you can live with possibly
-        // inaccurate results
-    }
+	function addClassName(element, cName) {
+		var regexp = new RegExp("(^|\\s)" + cName + "(\\s|$)");
+
+		if (element && !regexp.test(element.className)) {
+			cName = element.className + " " + cName;
+			element.className = cName.replace(/^\s+|\s+$/g, "");
+		}
+	}
+
+	function removeClassName(element, cName) {
+		var r = new RegExp("(^|\\s)" + cName + "(\\s|$)");
+
+		if (element) {
+			cName = element.className.replace(r, " ");
+			element.className = cName.replace(/^\s+|\s+$/g, "");
+		}
+	}
+
+	dom.addClassName = addClassName;
+	dom.removeClassName = removeClassName;
 }());
 
-tddjs.each = (function () {
-    // Returns an array of properties that are not exposed
-    // in a for-in loop
-    function unEnumerated(object, properties) {
-        var length = properties.length;
+tddjs.isOwnProperty = (function() {
+	var hasOwn = Object.prototype.hasOwnProperty;
 
-        for (var i = 0; i < length; i++) {
-            object[properties[i]] = true;
-        }
+	if (typeof hasOwn == "function") {
+		return function(object, property) {
+			return hasOwn.call(object, property);
+		};
+	} else {
+		// Provide an emulation if you can live with possibly
+		// inaccurate results
+	}
+}());
 
-        var enumerated = length;
+tddjs.each = (function() {
+	// Returns an array of properties that are not exposed
+	// in a for-in loop
+	function unEnumerated(object, properties) {
+		var length = properties.length;
 
-        for (var prop in object) {
-            if (tddjs.isOwnProperty(object, prop)) {
-                enumerated -= 1;
-                object[prop] = false;
-            }
-        }
+		for (var i = 0; i < length; i++) {
+			object[properties[i]] = true;
+		}
 
-        if (!enumerated) {
-            return;
-        }
+		var enumerated = length;
 
-        var needsFix = [];
+		for ( var prop in object) {
+			if (tddjs.isOwnProperty(object, prop)) {
+				enumerated -= 1;
+				object[prop] = false;
+			}
+		}
 
-        for (i = 0; i < length; i++) {
-            if (object[properties[i]]) {
-                needsFix.push(properties[i]);
-            }
-        }
+		if (!enumerated) {
+			return;
+		}
 
-        return needsFix;
-    }
+		var needsFix = [];
 
-    var oFixes = unEnumerated({}, ["toString", "toLocaleString", "valueOf",
-        "hasOwnProperty", "isPrototypeOf", "constructor",
-        "propertyIsEnumerable"]);
+		for (i = 0; i < length; i++) {
+			if (object[properties[i]]) {
+				needsFix.push(properties[i]);
+			}
+		}
 
-    var fFixes = unEnumerated(function () {
-    }, ["call", "apply", "prototype"]);
+		return needsFix;
+	}
 
-    if (fFixes && oFixes) {
-        fFixes = oFixes.concat(fFixes);
-    }
+	var oFixes = unEnumerated({}, [ "toString", "toLocaleString", "valueOf",
+			"hasOwnProperty", "isPrototypeOf", "constructor",
+			"propertyIsEnumerable" ]);
 
-    var needsFix = {
-        "object": oFixes,
-        "function": fFixes
-    };
+	var fFixes = unEnumerated(function() {
+	}, [ "call", "apply", "prototype" ]);
 
-    return function (object, callback) {
-        if (typeof callback != "function") {
-            throw new TypeError("callback is not a function");
-        }
+	if (fFixes && oFixes) {
+		fFixes = oFixes.concat(fFixes);
+	}
 
-        // Normal loop, should expose all enumerable properties
-        // in conforming browsers
-        for (var prop in object) {
-            if (tddjs.isOwnProperty(object, prop)) {
-                callback(prop, object[prop]);
-            }
-        }
+	var needsFix = {
+		"object" : oFixes,
+		"function" : fFixes
+	};
 
-        // Loop additional properties in non-conforming browsers
-        var fixes = needsFix[typeof object];
+	return function(object, callback) {
+		if (typeof callback != "function") {
+			throw new TypeError("callback is not a function");
+		}
 
-        if (fixes) {
-            var property;
+		// Normal loop, should expose all enumerable properties
+		// in conforming browsers
+		for ( var prop in object) {
+			if (tddjs.isOwnProperty(object, prop)) {
+				callback(prop, object[prop]);
+			}
+		}
 
-            for (var i = 0, l = fixes.length; i < l; i++) {
-                property = fixes[i];
+		// Loop additional properties in non-conforming browsers
+		var fixes = needsFix[typeof object];
 
-                if (tddjs.isOwnProperty(object, property)) {
-                    callback(property, object[property]);
-                }
-            }
-        }
-    };
+		if (fixes) {
+			var property;
+
+			for (var i = 0, l = fixes.length; i < l; i++) {
+				property = fixes[i];
+
+				if (tddjs.isOwnProperty(object, property)) {
+					callback(property, object[property]);
+				}
+			}
+		}
+	};
 }());
